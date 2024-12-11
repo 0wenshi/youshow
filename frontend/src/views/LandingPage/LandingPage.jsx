3f928eb00c66491583243ac55d34f86433496adc
owen <ekhen1223@gmail.com>
2024-12-11 15:46:35 +1300

    Updated landingpage

--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -14,9 +14,11 @@ function LandingPage() {
           src="/images/youshow.png"
           alt="YOU SHOW"
           className="transition-transform duration-300 hover:scale-110 mx-auto"
+          style={{ width: '600px' }}
         />
         <Link to="/homepage">
-          <button className="mt-32 bg-orange-500 hover:bg-orange-600 rounded-[39px] px-8 py-4 text-black font-bold text-lg flex items-center justify-center cursor-pointer transition-transform duration-300 hover:scale-105">
+          <button 
+          className="mt-32 bg-orange-500 hover:bg-orange-600 rounded-[39px] px-20 py-4 text-black font-bold text-3xl flex items-center justify-center cursor-pointer transition-transform duration-300 hover:scale-105">
             GET START
           </button>
         </Link>
