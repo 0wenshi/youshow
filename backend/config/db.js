8492d8198c52534e7cc504cbc1f420722b2fad38
owen <ekhen1223@gmail.com>
2024-12-24 11:57:06 +1300

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,29 @@
-require('dotenv').config();
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
   process.env.DB_PASSWORD || null,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST, // Public IP
+    port: process.env.DB_PORT || 3306, 
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true, 
+      },
+    },
+    logging: false,
   }
 );
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Database connected successfully.');
+    console.log('Connected to Google Cloud SQL successfully!');
   })
-  .catch((err) => {
-    console.error('Database connection failed:', err);
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
   });
 
 module.exports = sequelize;
